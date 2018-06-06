ded75d449804b57409eb7e54e76be56f791a93e0
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 20:21:21 +0000

    automatically backed up by learn

--- iterate_through_keys.rb
+++ iterate_through_keys.rb
@@ -1,9 +1,12 @@
 def iterate_through_keys
    new_summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012", :Atlanta => "1996"}
    new_summer_olympics.each_key do |key|
-   upcased_cities = ["#{key.upcase}"] 
-   puts upcased_cities
+     new_array = ["#{key.upcase}"]
+  # upcased_cities = ["#{key.upcase}"] 
+  # iterated_keys = upcased_cities
+  # upcased_cities = [] 
+  # puts upcased_cities
  end
 end
 
-puts iterate_through_keys
+iterate_through_keys
