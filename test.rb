71ab82f2d9156245743e8056fcdc92da84f45481
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 20:21:58 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,7 @@
+
+def add_a_key_value_pair
+    summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
+    summer_olympics[:Atlanta]= "1996"
+end
+
+puts add_a_key_value_pair
